94260ea257dcd47f9689959e77f5a494325b4418
Richard Opoku <richardopoku@expresspaygh.com>
2020-02-25 15:43:10 +0000

    initial commit on this tutorial

---
+++ src/main/java/com/second/seconnd/SeconndApplication.java
@@ -0,0 +1,13 @@
+package com.second.seconnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeconndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeconndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/second/seconnd/SeconndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.second.seconnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeconndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
